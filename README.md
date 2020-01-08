85def44e215eab92e485ee9e6f4ec6836f9b99e2
cindyweiss <weiss_cindy@yahoo.com>
2020-01-08 11:25:26 -0800

    we completed first feature now moving on to feature 2

--- README.md
+++ README.md
@@ -2,11 +2,21 @@
 
 Pair programmed with Blandine Dasilveira
 
-Number and name of feature: ________________________________
+Number and name of feature: ___________feature 1_-display images ____________________
 
-Estimate of time needed to complete: _____
+Estimate of time needed to complete: _1 hour____
 
-Start time: _____
+Start time: __10am___
+
+Finish time: _11:22am____
+
+Actual time needed to complete: ___1.22hrs__
+
+umber and name of feature: ___________feature 2_-Filter Images ____________________
+
+Estimate of time needed to complete: _1 hour____
+
+Start time: __11:30am___
 
 Finish time: _____
 
---
+++ js/app.js
@@ -0,0 +1,53 @@
+'use strict';
+
+let allPicture = [];
+
+
+function Picture(picObj) {
+  this.image_url = picObj.image_url;
+  this.title = picObj.title;
+  this.description = picObj.description;
+  this.keyword = picObj.keyword;
+  this.horns = picObj.horns;
+  allPicture.push(this);
+}
+
+
+Picture.prototype.render = function () {
+
+  //select all html in photo-template
+
+  const myTemplate = $('#photo-template').html();
+
+  // make a new section
+
+  const $newSection = $('<section></section>');
+
+  //put the html for photo-template inside a new section tag.
+
+  $newSection.html(myTemplate);
+
+  //find all elemnets and populate them
+
+  $newSection.find('h2').text(this.title);
+  $newSection.find('p').text(this.description);
+  $newSection.find('img').attr('src', this.image_url);
+  $newSection.find('img').attr('alt', this.title);
+  // append to the main
+  $('main').append($newSection);
+};
+
+
+
+
+
+
+
+$.ajax('/data/page-1.json', { method: 'GET', dataType: 'JSON' })
+  .then(data => {
+    data.forEach(function (newPic) {
+      let pic = new Picture(newPic);
+      pic.render();
+    });
+
+  });
