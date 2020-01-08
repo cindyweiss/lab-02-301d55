e1d7b2adee680729fb1b729cd02a52c660c2241d
cindyweiss <weiss_cindy@yahoo.com>
2020-01-08 12:58:30 -0800

    added the key arrays to it but now have to finish working on second portion

--- js/app.js
+++ js/app.js
@@ -1,7 +1,7 @@
 'use strict';
 
 let allPicture = [];
-
+let pictureKeysArray = [];
 
 function Picture(picObj) {
   this.image_url = picObj.image_url;
@@ -28,14 +28,16 @@ Picture.prototype.render = function () {
   $newSection.html(myTemplate);
 
   //find all elemnets and populate them
-
+  $newSection.find('section').addClass(this.keyword);
   $newSection.find('h2').text(this.title);
   $newSection.find('p').text(this.description);
   $newSection.find('img').attr('src', this.image_url);
-  $newSection.find('img').attr('alt', this.title);
+  $newSection.find('img').attr('alt', this.keyword);
   // append to the main
   $('main').append($newSection);
 };
+
+
 
 
 
@@ -48,6 +50,20 @@ $.ajax('/data/page-1.json', { method: 'GET', dataType: 'JSON' })
     data.forEach((newPic) => {
       let pic = new Picture(newPic);
       pic.render();
-    });
 
+    });
+    populateFilter();
   });
+
+
+//create a function that populates the keyword filter.
+
+const populateFilter = () => {
+  allPicture.forEach(imgObj => {
+    if (!pictureKeysArray.includes(imgObj.keyword)) {
+      pictureKeysArray.push(imgObj.keyword);
+    }
+  })
+
+}
+
